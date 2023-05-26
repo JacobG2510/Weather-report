c2ef81fcb5906c11ad78b0da9486ed6370ccdc68
blastking2510@gmail.com <blastking2510@gmail.com>
2023-05-25 20:57:39 -0700

    search bar

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,18 @@
 function getWeatherData() {
     // API configuration
-    const apiKey = 'df3a35b8272d9b66d05c6c9a1c3eda1c';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    var apiKey = 'df3a35b8272d9b66d05c6c9a1c3eda1c';
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
     // API request
     fetch(apiUrl)
       .then(response => response.json())
       .then(data => {
 
-        const temperature = data.main.temp;
-        const humidity = data.main.humidity;
-        const description = data.weather[0].description;
+        var temperature = data.main.temp;
+        var humidity = data.main.humidity;
+        var description = data.weather[0].description;
 
-          const container = document.getElementById('weather-container');
+          var container = document.getElementById('weather-container');
           container.innerHTML = `
             <p>Temperature: ${temperature} K</p>
             <p>Humidity: ${humidity}%</p>
@@ -20,8 +20,29 @@ function getWeatherData() {
           `;
         })
         .catch(error => {
-          console.log('An error occurred:', error);
+          console.log('please input city', error);
         });
+
     }
+    
+    //API Search bar
+    var container = document.getElementById('weather-container');
+    container.innerHTML = '';
+
+    var cityInput = document.getElementById('city-input');
+    var city = cityInput.value.trim();
+
+    function handleFormSubmit(event) {
+      event.preventDefault();
+
+      if(city) {
+        getWeatherData(city);
+      } 
+      
+      else {
+        var container = document.getElementById('weather-container');
+        container.innerHTML = '';
+      }
+    } 
 
     getWeatherData();
